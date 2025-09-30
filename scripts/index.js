fec4d7fc55060ef3dd0d32c5d4a9eee6165ecfdb
kevin Olalde <olaldekevin@gmail.com>
2025-09-29 20:12:41 -0700

    modals added and closed

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseButton =
+  editProfileModal.querySelector(".modal__close-btn");
+
+const newPostButton = document.querySelector(".profile__add-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
+
+editProfileButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostButton.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseButton.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
